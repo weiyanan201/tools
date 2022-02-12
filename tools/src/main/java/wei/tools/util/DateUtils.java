7b989644d9118c19389f25b7b453fee0ca933b89
魏亚楠 <wynan@workway.com.cn>
2022-02-13 01:52:03 +0800

    计算两个日期只差

--- tools/src/main/java/wei/tools/util/DateUtils.java
+++ tools/src/main/java/wei/tools/util/DateUtils.java
@@ -1,12 +1,11 @@
 package wei.tools.util;
 
-import org.apache.commons.lang3.StringUtils;
 import wei.tools.exception.ToolsException;
 
 import java.text.DateFormat;
-import java.text.Format;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,13 +17,13 @@ import java.util.regex.Pattern;
 public class DateUtils {
 
 
-    private static String dateFormat="yyyy-MM-dd";
+    private static String dateFormatStr="yyyy-MM-dd";
 
     //dateFormat 正则校验
     private static Pattern datePattern = Pattern.compile("^(20\\d{2})-((0{1}[0-9]{1})|(1{1}[0-2]{1}))-((0{1}[1-9]{1})|([1-2]{1}[0-9]{1})|(3{1}[0-1]{1})){1}$");
 
     public static String getDateFormat(){
-        return dateFormat;
+        return dateFormatStr;
     }
 
     public static String coverToUnSymbol(String dateStr){
@@ -32,7 +31,7 @@ public class DateUtils {
     }
 
     public static String getTodayStr(){
-        DateFormat format = new SimpleDateFormat(dateFormat);
+        DateFormat format = new SimpleDateFormat(dateFormatStr);
         Date today = new Date();
         return format.format(today);
     }
@@ -46,6 +45,28 @@ public class DateUtils {
         throw new ToolsException("日期格式不正确，请传入yyyy-MM-dd 格式");
     }
 
+    /**
+     *
+     * @param day1
+     * @param day2
+     * @return day1-day2
+     * @throws ParseException
+     */
+    public static int diffBetweenDays(String day1,String day2) throws ParseException {
+        checkFormat(day1);
+        checkFormat(day2);
+        DateFormat dateFormat = new SimpleDateFormat(dateFormatStr);
+        Date date1 = dateFormat.parse(day1);
+        Date date2 = dateFormat.parse(day2);
+        Calendar calendar1 = Calendar.getInstance();
+        calendar1.setTime(date1);
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.setTime(date2);
+
+        Long betweenDays = (calendar1.getTimeInMillis() - calendar2.getTimeInMillis()) / (1000 * 3600 * 24);//计算相差天数
+        return betweenDays.intValue();
+    }
+
     public static void main(String[] args) throws ParseException {
 
 //        String str = "[2022年1月4日的涨跌幅>0% (3309)2022年1月4日的涨跌幅<0% (1310)";
@@ -56,7 +77,8 @@ public class DateUtils {
 //            System.out.println(matcher.group(2));
 //        }
 
-        System.out.println(DateUtils.checkFormat("2021-13-12"));
+        int d = diffBetweenDays("2021-01-03","2022-01-03");
+        System.out.println(d);
 
     }
 
